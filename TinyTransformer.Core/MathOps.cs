0fd7dbb9627a14a14351f9f36824c9b263ec9e60
Konrad Cinkusz <konradcinkusz@gmail.com>
2025-09-24 15:47:12 +0200

    Add project files.

---
+++ TinyTransformer.Core/MathOps.cs
@@ -0,0 +1,116 @@
+﻿namespace TinyTransformer.Core;
+
+public static class MathOps
+{
+    //Matrix multiplication
+    public static float[,] MatMul(float[,] A, float[,] B)
+    {
+        int n = A.GetLength(0);
+        int m = A.GetLength(1);
+        int p = B.GetLength(1);
+
+        if (B.GetLength(0) != m) throw new ArgumentException("Incompatible size");
+
+        var C = new float[n, p];
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int k = 0; k < m; k++)
+            {
+                var aik = A[i, k];
+                for(int j=0;j< p; j++)
+                {
+                    C[i, j] += aik * B[i, j];
+                }
+            }
+        }
+
+        return C;
+    }
+
+    //Transpose martrix
+    public static float[,] Transpose(float[,] A)
+    {
+        int n = A.GetLength(0);
+        int m = A.GetLength(1);
+
+        var T = new float[m, n];
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j= 0; j< m; j++)
+            {
+                T[j,i] = A[i, j];
+            }
+        }
+
+        return T;
+    }
+
+    //ADD matrix (takes two 2D float arrays and returns
+    //their element-wise sum
+    //A and B must have the same shape (n x m)
+    public static float[,] Add(float[,] A, float[,] B)
+    {
+        int n = A.GetLength(0);
+        int m = A.GetLength(1);
+
+        if(B.GetLength(0) != n || B.GetLength(1) != m)
+        {
+            throw new ArgumentException("Shape mismatch");
+        }
+
+        var C = new float[n, m];
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                C[i, j] = A[i, j] + B[i, j];
+            }
+        }
+
+        return C;
+    }
+    
+    //adding a bias vector to each row of a matrix
+    //(a common step in neural networks)
+    public static float[,] AddBias(float[,] A, float[] b)
+    {
+        int n = A.GetLength(0);
+        int m = A.GetLength(1);
+
+        if (b.Length != m) 
+            throw new ArgumentException("Bias length mismatch.");
+
+        var C = new float[n, m];
+
+        for(int i = 0; i< n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                C[i, j] = A[i, j] + b[j];
+            }
+        }
+
+        return C;
+    }
+
+    //scalar–matrix multiplication method
+    public static float[,] Scal(float[,]A, float s)
+    {
+        int
+            n = A.GetLength(0);
+        int m = A.GetLength(1);
+        
+        var C = new float[n, m];
+
+        for( int i = 0; i< n; i++)
+            for(int j = 0; j< m; j++)
+            {
+                C[i, j] = A[i, j] * s;
+            }
+
+        return C;
+    }
+}
---
+++ TinyTransformer.Tests/MathOpsTests.cs
@@ -0,0 +1,111 @@
+using System;
+using FluentAssertions;
+using TinyTransformer.Core;
+using Xunit;
+
+namespace TinyTransformer.Tests;
+
+public class MathOpsTests
+{
+    private static void MatricesShouldBeApproximatelyEqual(float[,] actual, float[,] expected, float tol = 1e-5f)
+    {
+        actual.GetLength(0).Should().Be(expected.GetLength(0));
+        actual.GetLength(1).Should().Be(expected.GetLength(1));
+        for (int i = 0; i < actual.GetLength(0); i++)
+            for (int j = 0; j < actual.GetLength(1); j++)
+                actual[i, j].Should().BeApproximately(expected[i, j], tol);
+    }
+
+    [Fact]
+    public void MatMul_SmallExample_Works()
+    {
+        // A: 2x3, B: 3x2 -> C: 2x2
+        var A = new float[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+        var B = new float[,] { { 7, 8 }, { 9, 10 }, { 11, 12 } };
+
+        var C = MathOps.MatMul(A, B);
+
+        var expected = new float[,] {
+            { 58, 64 },        // [1*7 + 2*9 + 3*11, 1*8 + 2*10 + 3*12]
+            { 139, 154 }       // [4*7 + 5*9 + 6*11, 4*8 + 5*10 + 6*12]
+        };
+
+        MatricesShouldBeApproximatelyEqual(C, expected);
+    }
+
+    [Fact]
+    public void Transpose_Works()
+    {
+        var A = new float[,] { { 1, 2, 3 }, { 4, 5, 6 } }; // 2x3
+        var AT = MathOps.Transpose(A);                     // 3x2
+
+        var expected = new float[,] { { 1, 4 }, { 2, 5 }, { 3, 6 } };
+        MatricesShouldBeApproximatelyEqual(AT, expected);
+    }
+
+    [Fact]
+    public void Add_Works()
+    {
+        var A = new float[,] { { 1, 2 }, { 3, 4 } };
+        var B = new float[,] { { 5, 6 }, { 7, 8 } };
+
+        var C = MathOps.Add(A, B);
+
+        var expected = new float[,] { { 6, 8 }, { 10, 12 } };
+        MatricesShouldBeApproximatelyEqual(C, expected);
+    }
+
+    [Fact]
+    public void AddBias_AddsVectorToEachRow()
+    {
+        var A = new float[,] { { 1, 2, 3 }, { 4, 5, 6 } }; // 2x3
+        var b = new float[] { 10, 20, 30 };                // length 3
+
+        var C = MathOps.AddBias(A, b);
+
+        var expected = new float[,] { { 11, 22, 33 }, { 14, 25, 36 } };
+        MatricesShouldBeApproximatelyEqual(C, expected);
+    }
+
+    [Fact]
+    public void Scale_Works()
+    {
+        var A = new float[,] { { 1, -2 }, { 0.5f, 4 } };
+        var C = MathOps.Scal(A, 2f);
+
+        var expected = new float[,] { { 2, -4 }, { 1f, 8 } };
+        MatricesShouldBeApproximatelyEqual(C, expected);
+    }
+
+    // ----------------- error cases -----------------
+
+    [Fact]
+    public void MatMul_DimensionMismatch_Throws()
+    {
+        var A = new float[,] { { 1, 2 }, { 3, 4 } };       // 2x2
+        var B = new float[,] { { 1, 2, 3 } };              // 1x3 (mismatch)
+
+        Action act = () => MathOps.MatMul(A, B);
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Add_DimensionMismatch_Throws()
+    {
+        var A = new float[,] { { 1, 2 }, { 3, 4 } };
+        var B = new float[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+        Action act = () => MathOps.Add(A, B);
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void AddBias_LengthMismatch_Throws()
+    {
+        var A = new float[,] { { 1, 2, 3 } };
+        var b = new float[] { 1, 2 };
+
+        Action act = () => MathOps.AddBias(A, b);
+        act.Should().Throw<ArgumentException>();
+    }
+}
